429bf18a9e0290b2e047e9c62d099709e297a656
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-03-31 21:52:20 +0400

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,120 @@
 import { Hono } from "hono";
 import { LessonModel } from "./schemas/lesson";
 import connectDB from "./db";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+	"*",
+	cors({
+		origin: "http://localhost:5173",
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	}),
+);
 
 app.use("*", async (c, next) => {
 	await connectDB();
 	await next();
 });
+
+app.get("/lessons", async (c) => {
+	try {
+		const lessons = await LessonModel.find({ published: true });
+		console.log(lessons);
+		return c.json(lessons);
+	} catch (error) {
+		console.error("Error fetching lessons:", error);
+		return c.json({ error: "Failed to fetch lessons" }, 500);
+	}
+});
+
+app.get("/lessons/display", async (c) => {
+	try {
+		const lessons = await LessonModel.find(
+			{ published: true },
+			"title tags authors section",
+		);
+		return c.json(lessons);
+	} catch (e) {
+		console.error("Error fetching lessons:", e);
+		return c.json({ error: "Failed to fetch lessons" }, 500);
+	}
+});
+
+app.get("/lesson/:id", async (c) => {
+	const id = c.req.param("id");
+
+	try {
+		const lesson = await LessonModel.findById(id);
+		return c.json(lesson);
+	} catch (error) {
+		console.error("Error fetching lesson");
+		return c.json({ error: "Failed to fetch lessons" }, 500);
+	}
+});
+
+app.post("/lesson", async (c) => {
+	const body = await c.req.json();
+	try {
+		const lesson = body as Lesson;
+
+		const dbLesson = await LessonModel.create({
+			title: lesson.title,
+			published: lesson.published,
+			tags: lesson.tags,
+			authors: lesson.authors,
+			section: lesson.section,
+			steps: lesson.steps,
+		});
+
+		return c.json(dbLesson, 201);
+	} catch (e) {
+		return c.json({ error: "Failed to fetch lessons" }, 500);
+	}
+});
+
+app.delete("/lesson/:id", async (c) => {
+	const id = c.req.param("id");
+	try {
+		const lesson = await LessonModel.findByIdAndDelete(id);
+		return c.json({ message: "Lesson deleted successfully" });
+	} catch (e) {
+		console.log(e);
+		return c.json({ error: "Failed to delete the lesson" }, 500);
+	}
+});
+
+app.put("/lesson/:id", async (c) => {
+	const id = c.req.param("id");
+	const body = await c.req.json();
+	try {
+		const lesson = body as Lesson;
+
+		const dbLesson = await LessonModel.findByIdAndUpdate(id, lesson);
+
+		return c.json({ message: "Lesson updated successfully" });
+	} catch (e) {
+		console.error(e);
+		return c.json({ error: "Failed to update the lesson" });
+	}
+});
+
+app.patch("/lesson/:id", async (c) => {
+	const id = c.req.param("id");
+	const body = await c.req.json();
+	try {
+		const dbLesson = await LessonModel.findByIdAndUpdate(id, body);
+		return c.json({ message: "Lesson updated successfully" });
+	} catch (e) {
+		console.error(e);
+		return c.json({ error: "Failed to update the lesson" });
+	}
+});
+
+export default app;
 
 interface Step {
 	title: string;
@@ -39,135 +146,3 @@ interface Lesson {
 	section: string;
 	steps?: Step[];
 }
-
-app.get("/lessons", async (c) => {
-	try {
-		const lessons = await LessonModel.find({ published: true });
-		console.log(lessons);
-		return c.json(lessons, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (error) {
-		console.error("Error fetching lessons:", error);
-		return c.json({ error: "Failed to fetch lessons" }, 500);
-	}
-});
-
-app.get("/lessons/display", async (c) => {
-	try {
-		const lessons = await LessonModel.find(
-			{ published: true },
-			"title tags authors section",
-		);
-		return c.json(lessons, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (e) {
-		console.error("Error fetching lessons:", e);
-		return c.json({ error: "Failed to fetch lessons" }, 500);
-	}
-});
-
-app.get("/lesson/:id", async (c) => {
-	const id = c.req.param("id");
-
-	try {
-		const lesson = await LessonModel.findById(id);
-		return c.json(lesson, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (error) {
-		console.error("Error fetching lesson");
-		return c.json({ error: "Failed to fetch lessons" }, 500);
-	}
-});
-
-app.post("/lesson", async (c) => {
-	const body = await c.req.json();
-	try {
-		const lesson = body as Lesson;
-
-		const dbLesson = await LessonModel.create({
-			title: lesson.title,
-			published: lesson.published,
-			tags: lesson.tags,
-			authors: lesson.authors,
-			section: lesson.section,
-			steps: lesson.steps,
-		});
-
-		return c.json(dbLesson, 201, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (e) {
-		return c.json({ error: "Failed to fetch lessons" }, 500);
-	}
-});
-
-app.delete("/lesson/:id", async (c) => {
-	const id = c.req.param("id");
-	try {
-		const lesson = await LessonModel.findByIdAndDelete(id);
-		return c.json({ message: "Lesson deleted successfully" });
-	} catch (e) {
-		console.log(e);
-		return c.json({ error: "Failed to delete the lesson" }, 500);
-	}
-});
-
-app.put("/lesson/:id", async (c) => {
-	const id = c.req.param("id");
-	const body = await c.req.json();
-	try {
-		const lesson = body as Lesson;
-
-		const dbLesson = await LessonModel.findByIdAndUpdate(id, lesson);
-
-		return c.json({ message: "Lesson updated successfully" }, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (e) {
-		console.error(e);
-		return c.json({ error: "Failed to update the lesson" });
-	}
-});
-
-app.patch("/lesson/:id", async (c) => {
-	const id = c.req.param("id");
-	const body = await c.req.json();
-	try {
-		const dbLesson = await LessonModel.findByIdAndUpdate(id, body);
-		return c.json({ message: "Lesson updated successfully" }, {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:5173",
-				"Access-Control-Allow-Methods": "*",
-				"Access-Control-Allow-Headers": "*",
-			},
-		});
-	} catch (e) {
-		console.error(e);
-		return c.json({ error: "Failed to update the lesson" });
-	}
-});
-
-export default app;
